864eeca6dc060fee8321a1c0d918d8a3373730bc
goretti16kate <gorettikate@gmail.com>
2021-11-01 21:26:46 +0300

    revising java

---
+++ jaiva/Main.java
@@ -0,0 +1,8 @@
+public class Main{
+
+    public static void main(String[] args){
+       System.out.println("First line of our app"); 
+       System.out.println("Second line of our app"); 
+       System.out.println("Third line of our app"); 
+    }
+}
