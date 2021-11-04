80623c097238a58f09d9bf0ec44deeab0d8f3c3c
goretti16kate <gorettikate@gmail.com>
2021-11-04 22:31:41 +0300

    birthday card

---
+++ kotlin/birthday.kt
@@ -0,0 +1,19 @@
+fun main() {
+    val name = "Rover"
+    val age = 5 * 365
+    println("Happy Birthday, ${name}!")
+
+        // Let's print a cake!
+    println("   ,,,,,   ")
+    println("   |||||   ")
+    println(" =========")
+    println("@@@@@@@@@@@")
+    println("{~@~@~@~@~}")
+    println("@@@@@@@@@@@")
+    
+    // This prints an empty line.
+    println("")
+
+    println("You are already ${age} days old, ${name}!")
+    println("${age} days old is the very best age to celebrate!")
+}
---
+++ kotlin/practice.kt
@@ -0,0 +1,6 @@
+// Variable declaration
+// variable types : Byte, Short, Long, Float, Double.
+//var count: Int = 10 // variable declaration for a value that changes
+//val languageName: String = "Kotlin" //this is a variable whose value never changes
+// You cannot call functions that are not part o the inferred type.
+//val upperCaseName = languageName.toUpperCase()
